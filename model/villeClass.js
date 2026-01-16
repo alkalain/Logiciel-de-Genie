e55b1fd3720442f2e4593324bfc104fa717c20b3
louane <louane.mainguet@gmail.com>
2026-01-16 21:09:48 +0100

    clean villeClass.js

--- model/villeClass.js
+++ model/villeClass.js
@@ -10,20 +10,6 @@ class villeClass {
     getPriceAllProgramations() {
         return this.programmations.reduce((total, prog) => total + prog.prix, 0);
     }
-    AchatBilletVille() {
-        this.programmations.forEach(element => {
-            element.capacite --;
-        });
-        return this.getPriceAllProgramations();
-    }
-
-    AchatBilletLieu(lieu) {
-        lieu = this.programmations.find(prog => prog.lieu === lieu);
-        if (lieu && lieu.capacite > 0) {
-            lieu.capacite --;
-        }
-        return lieu.prix();
-    }
 
     getMinCapacity() {
         if (this.programmations.length === 0) return 0;
