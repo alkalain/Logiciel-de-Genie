4fd285bc45d5f69638f746486831a8ce863932c0
louane <louane.mainguet@gmail.com>
2026-01-16 15:59:42 +0100

    modification de troupe pour ajouter des artistes

--- model/troupeClass.js
+++ model/troupeClass.js
@@ -2,9 +2,22 @@ class troupeClass {
     constructor(id, nom) {
         this.id = id;
         this.nom = nom;
+        this.artistes = [];
         this.spectacles = [];
     }
+    addArtiste(artiste) {
+        this.artistes.push(artiste);
+    }
+
     addSpectacle(spectacle) {
         this.spectacles.push(spectacle);
+    }
+
+    suppSpectacle(spectacle) {
+        this.spectacles = this.spectacles.filter(s => s !== spectacle);
+    }
+    
+    getInfos() {
+        return `Troupe [ID: ${this.id}, Nom: ${this.nom}, spectacle: ${this.spectacles.join(', ')}]`;
     }
 }
